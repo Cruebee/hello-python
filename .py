33e9a05ba24de787426d6fc4cc08d349482549ca
Crue Jameson <ericcjameson@gmail.com>
2020-10-21 10:48:52 -0600

    first commit learning python

---
+++ .py
@@ -0,0 +1,12 @@
+character_name = "Timothy"  # This is a string
+character_age = 35  # This is an integer
+is_male = False  # This is a boolean
+
+print("There once was a man named " + character_name + ", ")
+# In order to print an integer as a string you need to use
+# str() and wrap your variable like so:
+print("he was " + str(character_age) + " years old. ")
+
+character_name = "Mickey"
+print("He really liked the name " + character_name + ", ")
+print("but didn't like being " + str(character_age) + ".")
